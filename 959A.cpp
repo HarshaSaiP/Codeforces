454dcbedb2b9888be3799640a8f2ed6115d9444c
HarshaSai03 <harshap3712@gmail.com>
2026-09-26 07:34:29 +0530

    26-09-2026

---
+++ 959A.cpp
@@ -0,0 +1,11 @@
+//https://codeforces.com/problemset/problem/959/A
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin>>n;
+    if(n%2==0) cout<<"Mahmoud";
+    else cout<<"Ehab";
+}
